5056d45156409487cdf9867744d40f919cd6556d
Kshitijgith <kshitij.kamble@vit.edu.in>
2025-02-27 10:20:35 +0530

    working

--- Server/index.js
+++ Server/index.js
@@ -44,6 +44,6 @@ app.get("/:shortId",GetLink );
 
 // ✅ Start Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`App running on PORT ${PORT}`);
 });
